b09b429ce8a496d699de86d496e75f3e1b687e33
noahshlesinger <95305584+noahshlesinger@users.noreply.github.com>
2022-03-29 14:41:59 -0700

    Update Employee_Database_Challenge.sql

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,12 +1,16 @@
 
-SELECT DISTINCT ON (emp_no) e.birth_date,
-e.emp_no,
+
+-- Attempt #1
+SELECT DISTINCT ON (emp_no) e.emp_no,
 e.first_Name,
 e.last_name,
-ti.title
-INTO retirement_titles
+ti.title,
+ti.from_date,
+ti.to_date
+-- INTO retirement_titles
 FROM employees as e
 INNER JOIN titles AS ti
 ON (e.emp_no = ti.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no, ti.from_date DESC;
+
