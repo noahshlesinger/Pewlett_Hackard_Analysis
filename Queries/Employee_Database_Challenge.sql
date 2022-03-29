74eda77a862c1610783b7d6a06399b56e6aefb12
noahshlesinger <95305584+noahshlesinger@users.noreply.github.com>
2022-03-29 14:38:45 -0700

    retirement list by titles, removed duplicates

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,10 +1,12 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+SELECT DISTINCT ON (emp_no) e.birth_date,
+e.emp_no,
+e.first_Name,
+e.last_name,
+ti.title
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no, ti.from_date DESC;
