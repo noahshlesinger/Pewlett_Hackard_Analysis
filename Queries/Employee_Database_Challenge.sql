ed00724eca6bfa148ac9d200032100d1d35c8f6a
noahshlesinger <95305584+noahshlesinger@users.noreply.github.com>
2022-03-30 19:52:11 -0700

    Adding eligible mentors list

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,6 +1,6 @@
 
 
--- Attempt #1
+-- Deliverable #1
 SELECT DISTINCT ON (emp_no) e.emp_no,
 e.first_Name,
 e.last_name,
@@ -14,3 +14,22 @@ ON (e.emp_no = ti.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no, ti.from_date DESC;
 
+
+
+-- Deliverable #2
+SELECT DISTINCT ON (emp_no)
+e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no
